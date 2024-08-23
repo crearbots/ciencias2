1fbfe86f79f38acf7f32e83afb875b57d63f5804
Juanse <jsrvjuan@gmail.com>
2024-08-23 16:49:31 -0500

    Se crea la clase Transaccion, se debe crear el metodo HashUtil de la clase que gestione los hash

---
+++ demo/src/main/java/com/example/blockchain/Transaccion.java
@@ -0,0 +1,48 @@
+package com.example.blockchain;
+
+import com.example.productos.Producto;
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class Transaccion {
+    private String idTransaccion;
+    private Producto[] productos;
+    private long timestamp;
+    private String hashAnterior;
+    private String hashActual;
+
+    public Transaccion(String idTransaccion, Producto[] productos, long timestamp, String hashAnterior) throws NoSuchAlgorithmException {
+        this.idTransaccion = idTransaccion;
+        this.productos = productos;
+        this.timestamp = timestamp;
+        this.hashAnterior = hashAnterior;
+        this.hashActual = generarHash();
+    }
+
+    private String generarHash() throws NoSuchAlgorithmException {
+        String datos = idTransaccion + Arrays.toString(productos) + timestamp + hashAnterior;
+        return HashUtil.generarSHA256(datos);
+    }
+    
+    public String getHashActual() {
+        return hashActual;
+    }
+
+    public String getHashAnterior() {
+        return hashAnterior;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaccion{" +
+                "idTransaccion=" + idTransaccion + '\'' +
+                ", productos=" + Arrays.toString(productos) +
+                ", timestamp=" + timestamp +
+                ", hashAnterior='" + hashAnterior + '\'' +
+                ", hashActual='" + hashActual + '\'' +
+                '}';
+    }
+}
